1cee4f414429b38e23b73b01545e36c786eea87f
Alexanderlacuna <alexanderkabua@gmail.com>
2020-07-20 22:59:59 +0300

    added bew page membership/index for allowing members to subscribe

---
+++ pages/membership/index.vue
@@ -0,0 +1,224 @@
+<template>
+<div class="">
+
+<div class="container ">
+  <div class="flex ">
+      <div > 
+        <div>
+  <h3 class="text-5xl font-semibold leading-tight">Unlimited access for the insatably curious</h3>
+    </div>
+
+    <div class="py-8">
+        <p class="text-left">Become a Medium member for $5/month or $50/year and get unlimited access to the 
+            smartest writers and biggest ideas you won’t find anywhere else.</p>
+    </div>
+
+<div class="space-y-4"> 
+
+    <div >
+
+        <button  class="px-10 text-center py-3 border border-gray-700">
+              <span class="text-green-600 px-2"><font-awesome-icon :icon="['fab','google']"/></span>
+            Continue with Google</button>
+      
+    </div>
+
+    <div>
+
+        <button class="px-8 py-3  text-center border border-gray-700">
+            <span  class="text-blue-700 px-2"><font-awesome-icon :icon="['fab','facebook-square',]"/></span>
+            Continue with Facebook</button>
+        
+    </div>
+
+    <div>
+
+        <h2 class="font-semibold  py-4 text-lg">Have an  account?<span class="text-green-600">Sign in</span></h2>
+    </div>
+</div>
+  </div>
+
+    <div>
+
+        <img src="https://miro.medium.com/max/980/1*90cu6c0u4ZL8H8ic1woZDw@2x.png" alt="">
+        
+
+    </div>
+  </div>
+</div>
+
+<div class="containerb bg-blue-100">
+    <div class="text-center  max-w-4xl  mx-auto  py-4">
+        <h3  class="font-semibold pb-10">thousands of perspectives ,One Home</h3>
+
+        <div class="pb-10">
+            <h3>Your favorite writers, thinkers, and experts are publishing their best work on Medium. And you’ll get access to all of it as a member. Here’s a sample of the diverse,
+                 bold thinking you can expect:</h3>
+        </div>
+  </div>
+        <div class="  grid grid-cols-12 gap-1 overflow-x-scroll">
+            <div class="col-span-2 ">
+             <img src="https://miro.medium.com/fit/c/524/644/1*EQLON9rFPaejG9oqz2iD2w.png" alt="">
+            </div>
+             <div class="col-span-2 ">
+             <img src="https://miro.medium.com/fit/c/524/644/1*EQLON9rFPaejG9oqz2iD2w.png" alt="">
+            </div>
+             <div class="col-span-2 ">
+             <img src="https://miro.medium.com/fit/c/524/644/1*EQLON9rFPaejG9oqz2iD2w.png" alt="">
+            </div>
+           
+             <div class="col-span-2 ">
+             <img src=" https://miro.medium.com/fit/c/524/644/1*26m49m69mnN1j49tn0-ffQ.png" alt="">
+            </div>
+            <div class="col-span-2">
+                <img src="https://miro.medium.com/fit/c/524/644/1*kCPA4jZRVEwcNGDkMulJZg.png" alt="">
+            </div>
+
+            <div  class="col-span-2">
+                <img src="https://miro.medium.com/fit/c/524/644/1*kCPA4jZRVEwcNGDkMulJZg.png" alt="">
+            </div>
+
+            <div class="col-span-2">
+                <img src="https://mirohttps://miro.medium.com/fit/c/524/644/1*kCPA4jZRVEwcNGDkMulJZg.png.medium.com/fit/c/524/644/1*kCPA4jZRVEwcNGDkMulJZg.png" alt="">
+            </div>
+            <div class="col-span-2">
+                <img src="https://miro.medium.com/fit/c/524/644/1*kCPA4jZRVEwcNGDkMulJZg.png" alt="">
+            </div>
+            
+            
+            
+        </div>
+
+  
+</div>
+
+<div class="containerb py-24 px-20">
+
+<div class="grid grid-cols-12 ">
+
+    <div class="col-span-4">
+     <div>
+          <h3 class="text-3xl  font-semibold ">What Medium members are saying</h3>
+     </div>
+    </div>
+
+    <div class="col-span-8 h-full -ml-12">
+
+        <div class=" bg-opacity-75  control-height bg-green-100  py-16 pl-24 pr-10">
+         <p  class="text-left font-semibold  text-xl">
+                 <q>
+                        Medium is a great place to read and learn from a wide range of authors. It’s not muddied up by ads. It’s not fake news. 
+           It feels like one of the few pure places to go online.
+                 </q>
+         </p>
+        </div>
+
+    </div>
+    
+</div>
+
+</div>
+
+<div class="control-height grid grid-cols-12 px-16">
+
+    <div class="col-span-6">
+        <div class="contol-image">
+        <img class="" src="https://miro.medium.com/max/400/1*koXya9xnnxs4lmo6LpVbNw.png" alt="">
+
+    </div>
+    </div>
+
+    <div class="col-span-6">
+
+        <div>
+            <h3 class="font-semibold text-lg">Your membership powerful</h3>
+            <div>
+                <h1 class="text-5xl text-left leading-relaxed py-10">
+                    Directly reward the stories and authors you love
+                </h1>
+
+                <p class="py-10"> 
+                    We believe in compensating writers for the value they provide to you, not the fleeting attention they attract for advertisers. That’s why your membership 
+                    fee directly supports the authors you spend time reading.
+                </p>
+
+                <p>
+                    You already love a good story. Ever thought about writing one?
+                </p>
+
+                <div class="py-8 text-green-700 font-semibold">
+                    <a href="">Learn writing on medium</a>
+                </div>
+        
+            </div>
+        </div>
+
+    </div>
+
+</div>
+
+<div class="control-last grid grid-cols-12 bg-gray-200 py-32 px-16  gap-2">
+<div class="col-span-5">
+    <div>
+    <h3 class="text-5xl font-semibold">Thanks for reading</h3>
+    <h3 class="text-5xl font-semibold">Ready to upgrade members</h3>
+</div>
+
+<div>
+    <div class="flex flex-col space-y-6 mt-8  items-start ">
+    <button class="py-4 border border-gray-700 lg:w-1/2 ">Continue with google</button>
+    <button class=" border py-4 border-gray-700 lg:w-1/2">Continue with facebook</button>
+
+
+
+</div>
+</div>
+
+<div class="py-8">
+    <a href=""  class="text-lg">Have an account?<span class="text-green-700">Sign in</span></a>
+</div>
+
+</div>
+<div  class=" col-span-7">
+    <img  class="block" src="https://miro.medium.com/max/1800/1*vZ8miTqyE3eUnBqQjvLJeA.png" alt="">
+</div>
+</div>
+
+</div>
+</template>
+
+
+
+<style scoped>
+.control-last{
+    
+    min-height: 100vh;
+    /* background: red; */
+}
+.control-image{
+    height: 30vh;
+}
+.control-height{
+    height: 100vh;
+
+
+}
+.container{
+    min-height: 80vh;
+    padding-top:20vh;
+    
+    display: flex;
+    /* justify-items: center;
+     */
+    justify-content: center;
+    align-items: center;
+
+    max-width: 1200px;
+    margin: 0 auto;
+    /* background: red; */
+}
+
+.containerb{
+    min-height: 100vh;
+}
+</style>
